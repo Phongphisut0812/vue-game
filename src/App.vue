b7aa1566995903575c647cedcae8c2c0a5601739
Phongphisut0812 <68526077+Phongphisut0812@users.noreply.github.com>
2020-08-28 21:57:09 +0700

    new

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,97 @@
 <template>
   <div id="app">
-   <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <Ironman/>
+    <game
+      @H_name="h_name"
+      @H_hp="h_hp"
+      @H_image="h_image"
+      @M_name="m_name"
+      @M_hp="m_hp"
+      @M_image="m_image"
+      @P_atk="p_atk"
+      @M_atk="m_atk"
+      @P_SATK="p_satk"
+      @M_SPATK="m_satk"
+    ></game>
+    <div class="row">
+      <div class="col-md-4">
+        <h1>NAME :{{he_name}} HP : {{he_hp}}</h1>
+        <div>
+          <img :src="h_img" alt class="img-fluid image" :width="he_hp + 'vw' " />
+        </div>
+      </div>
+      <div class="col-md-4">
+        <img src ="./assets/vs.png"  />
+      </div>
+      <div class="col-md-4">
+        <h1>NAME :{{mon_name}} HP : {{mon_hp}}</h1>
+        <div>
+          <img :src="m_img" alt class="img-fluid image" :width="mon_hp + 'vw' " />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Ironman from "./components/Ironman.vue";
-
+import game from "./components/game.vue";
 export default {
   name: "App",
   components: {
-    Ironman,
+    game,
+  },
+  data: function () {
+    return {
+      he_name: "",
+      mon_name: "",
+      he_hp: 0,
+      mon_hp: 0,
+      attack: 0,
+      h_img: "",
+      m_img: "",
+    };
+  },
+  methods: {
+    h_name(value) {
+      this.he_name = value;
+      console.log("emit", this.he_name);
+    },
+    h_hp(value) {
+      this.he_hp = value;
+      console.log("emit", this.he_hp);
+    },
+    h_image(value) {
+      this.h_img = value;
+      console.log("emit", this.h_img);
+    },
+    //monster
+    m_name(value) {
+      this.mon_name = value;
+    },
+    m_hp(value) {
+      this.mon_hp = value;
+    },
+    m_image(value) {
+      this.m_img = value;
+    },
+    //attack hero
+    p_atk(value) {
+      this.attack = value;
+      this.mon_hp -= this.attack;
+      console.log("emit", this.mon_hp);
+    },
+    p_satk(value) {
+      this.attack = value;
+      this.mon_hp -= this.attack;
+    },
+    //attack monster
+    m_atk(value) {
+      this.attack = value;
+      this.he_hp -= this.attack;
+    },
+    m_satk(value) {
+      this.attack = value;
+      this.he_hp -= this.attack;
+    },
   },
 };
 </script>
@@ -23,7 +102,12 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #ffffff;
+}
+.playGameScreen {
+  background-image: url("");
+  background-repeat: no-repeat;
+  background-size: 100% 100%;
+  height: 100vh;
 }
 </style>
---
+++ src/components/game.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+      <button v-on:click="Start" class="button btn btn-warning" id="start">Start</button>
+      <button v-on:click="Attack" class="button btn btn-warning" id="atk">Attack</button>
+      <button v-on:click="Special" class="button btn btn-warning" id="atk">Special Attack</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      /*
+      aplayer: { name: "kko", hp: 0, image: "0" },
+      mmonster: { name: "ook", hp: 0, image: "g" },
+      */
+      
+      player: [
+        { name: "Iron man", hp: 280, image: require("../assets/ironman.png") },
+        { name: "Thor", hp: 300, image: require("../assets/Thor.png") },
+        { name: "Loki", hp: 200, image: require("../assets/Loki1.png") },
+        { name: "Captain", hp: 280, image: require("../assets/Captain.png") },
+        { name: "The Hulk", hp: 300, image: require("../assets/Hulk.png") },
+      ],
+      monster: [
+        { name: "Thanos", hp: 300, image: require("../assets/Thanos1.png") },
+        { name: "Black Dwarf", hp: 250, image: require("../assets/Black.png") },
+        { name: "Corvus Glaive", hp: 200, image: require("../assets/Corvus.png") },
+        { name: "Ebony maw", hp: 210, image: require("../assets/Ebony.png") },
+        { name: "Proxi Midnight", hp: 240, image: require("../assets/Procxima.png") },
+      ],
+    };
+  },
+
+  methods: {
+    randomNO: function (min, max) {
+      return Math.max(Math.floor(Math.random() * max) + 1, min);
+    },
+    Start: function () {
+      //1.random player 1-3
+      //2.random monster 1-3
+      this.aplayer = this.randomNO(1, 5) - 1; //{..}
+      this.mmonster = this.randomNO(1, 5) - 1;
+
+      this.name_player = this.player[this.aplayer].name;
+      this.$emit("H_name", this.name_player);
+      console.log("emit", this.name_player);
+      this.hp_player = this.player[this.aplayer].hp;
+      this.$emit("H_hp", this.hp_player);
+      console.log("hp", this.hp_player);
+      this.image_player = this.player[this.aplayer].image;
+      this.$emit("H_image", this.image_player);
+      console.log("im", this.image_player);
+
+      this.name_monster = this.monster[this.mmonster].name;
+      console.log("emit", this.name_monster);
+      this.$emit("M_name", this.name_monster);
+      this.hp_monster = this.monster[this.mmonster].hp;
+      this.$emit("M_hp", this.hp_monster);
+      this.image_monster = this.monster[this.mmonster].image;
+      this.$emit("M_image", this.image_monster);
+    },
+    Attack: function () {
+      this.atk = this.randomNO(3, 10);
+      this.$emit("P_atk", this.atk);
+      console.log("Attack :", this.atk);
+
+      this.atk = this.randomNO(3, 10);
+      this.$emit("M_atk", this.atk);
+      console.log("Atk back" ,this.atk);
+ 
+    },
+    Special: function () {
+      this.s_atk = this.randomNO(10, 20);
+      this.$emit("P_SATK", this.s_atk);
+      console.log("Special Attack :", this.s_atk);
+
+      this.s_atk = this.randomNO(10, 20);
+      this.$emit("M_SPATK", this.s_atk);
+      console.log("SPATK back", this.s_atk);
+    },
+  },
+};
+</script>
+
+<style>
+.button {
+  border: none;
+  color: white;
+  padding: 10px 40 px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+</style>
